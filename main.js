f03957ef5484f693ddb492cf8dfde7800750b9e6
David Espinoza <deerhou@gmail.com>
2025-07-15 22:19:07 -0500

    enhance POP management check for POP alive and max to spawn

--- main.js
+++ main.js
@@ -1,80 +1,88 @@
 // IMPORTS
-const { cleanMemory, getBody, getName } = require('utils');
+const { cleanMemory, getBody, getName, getSourcesBySpawn } = require('utils');
 const roleHarvester = require('role.harvester')
 const roleUpgrader = require('role.upgrader')
 const roleBuilder = require('role.builder')
 
 // VARIABLES
+let actualTier = "tier" + 1;
 const spawnerName = 'Spawn1'
 const spawner = Game.spawns[spawnerName]
+const [energy1, energy2, energy3] = getSourcesBySpawn(spawnerName)
+// console.log(energy1);
 
-const numberForRole = {
-    harvester: 2,
-    builder: 1,
-    // upgrader:5,
-    miner: 0,
-    carrier: 0,
-    // soldier:8,
-    // archers:0,
+// Numero Actual the creeps y numero deseado
+let POP = {
+	isFull: false,
+	alive: {},
+	expected: {
+		// harvester: 2,
+		// builder: 1,
+		// upgrader:5,
+		miner: 0,
+		carrier: 0,
+		// soldier:8,
+		// archers:0,
+	},
 }
+const ROLES = Object.keys(POP.expected);
+Object.entries(POP.expected).forEach(([role, minWorkers]) => {
+	POP.alive[role] = _.filter(Game.creeps, (creep) => creep.memory.role == role).length;
+});
 
 
 // FUNCTIONS
 
-// creep parts ([string] body, string name, object opts
-
-// spawner.spawnCreep([WORK, CARRY, MOVE], Game.time+'Worker1', {
-//     memory: {role: 'harvester'}
-// });
 module.exports.loop = function () {
-    cleanMemory()
+	cleanMemory()
 
-    Object.entries(numberForRole).forEach(([role, minWorkers]) => {
-        let numberAlive = _.filter(Game.creeps, (creep) => creep.memory.role == role);
-        if (numberAlive.length >= minWorkers) return;
-        spawner.spawnCreep(getBody(1, role), getName(role), { memory: { role } })
-    });
+	if (!POP.isFull && !spawner.spawning) {
+		for (const role of ROLES) {
+			console.log(role, POP.alive[role], POP.expected[role], POP.alive[role] >= POP.expected[role]);
 
-    if (spawner.spawning) {
-        const spawningCreep = Game.creeps[spawner.spawning.name];
-        spawner.room.visual.text(
-            '🛠️' + spawningCreep.memory.role,
-            spawner.pos.x + 1,
-            spawner.pos.y,
-            { align: 'left', opacity: 0.8 });
-    }
+			if (POP.alive[role] < POP.expected[role]) {
+				spawner.spawnCreep(getBody(actualTier, role), getName(role), { memory: { role } })
+				POP.alive[role]++;
+			}
+		}
+	}
 
-    for (let name in Game.creeps) {
-        const creep = Game.creeps[name];
+	if (spawner.spawning) {
+		const spawningCreep = Game.creeps[spawner.spawning.name];
+		spawner.room.visual.text(
+			'🛠️' + spawningCreep.memory.role,
+			spawner.pos.x + 1,
+			spawner.pos.y,
+			{ align: 'left', opacity: 0.8 });
+	}
 
-        switch (creep.memory.role) {
+	for (let name in Game.creeps) {
+		const creep = Game.creeps[name];
 
-            case 'harvester':
-                roleHarvester.run(creep);
-                break;
-            case 'upgrader':
-                roleUpgrader.run(creep);
-                break;
-            case 'builder':
-                roleBuilder.run(creep);
-                break;
-            case 'soldier':
-                roleSoldier.run(creep);
-                break;
-        }
+		switch (creep.memory.role) {
 
-    }
+			case 'harvester':
+				roleHarvester.run(creep);
+				break;
+			case 'upgrader':
+				roleUpgrader.run(creep);
+				break;
+			case 'builder':
+				roleBuilder.run(creep);
+				break;
+			case 'soldier':
+				roleSoldier.run(creep);
+				break;
+		}
+
+	}
 }
 
 
-// var harvesters = _.filter(Game.creeps, {
-//     memory: {role: 'harvester'}
-// });
-
 /* Gestion de aldea
 - spawner creeps
-    - roles (default, builder, miner, upgrader, carrier)
-    - 
+	- roles (default, builder, miner, upgrader, carrier)
+	- 
 - creeps do tasks
 - manage population
 
